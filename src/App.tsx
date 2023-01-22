a3ad5130b18f563243fa3b737a25814c1ed4d0d3
abidh <abidhone@gmail.com>
2023-01-22 22:56:24 +0100

    Basic functionality of validatin date field based on locale selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import 'moment/min/locales'; // importing all locales
 
-function App() {
+function getBrowserLanguage() {
+  if (navigator.languages !== undefined) return navigator.languages[0];
+  return navigator.language;
+}
+
+export default function App() {
+  const [locale, setLocale] = useState(getBrowserLanguage());
+  const [date, setDate] = useState('');
+
+  const [, updateState] = React.useState<object>();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+
+  const dateFormat = moment.localeData().longDateFormat('L')
+
+  useEffect(() => {
+    moment.locale(locale);
+    forceUpdate();
+  }, [forceUpdate, locale]);
+
+  const validateDate = (date: string) => {
+    return moment(
+      date,
+      dateFormat,
+      true
+    ).isValid();
+  };
+
+  const handleLocaleChange = ({target: {value}}: React.ChangeEvent<HTMLSelectElement>) => {
+    setLocale(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: 20}}>
+      <select name="locales" onChange={handleLocaleChange} value={locale}>
+        <option value="en-GB">English UK</option>
+        <option value="en-US">English US</option>
+        <option value="ja">Japanese</option>
+        <option value="de">German</option>
+      </select>
+      <br />
+      <br />
+      Expected format: {dateFormat}
+      <br />
+      <br />
+      <input value={date} onChange={(e) => setDate(e.target.value)} />
+      <br />
+      {!!date && `strict eaqual: ${validateDate(date) ? 'valid' : 'not valid'}`}
     </div>
   );
 }
-
-export default App;
